d993b19fe10456b9df5c40766c6009e60d7e96fc
zhaoshengzhi <iloveyesterday@outlook.com>
2018-03-13 12:39:08 +0800

    用户

--- src/components/Page/UserInfo/UserInfo.vue
+++ src/components/Page/UserInfo/UserInfo.vue
@@ -1,10 +1,18 @@
 <template>
   <transition name="user-info-move">
-    <div class="user-info" v-show="isShow">
+    <div class="user-info" v-if="user!==null" v-show="isShow">
       <header>
         <div class="close" @click="hide"><</div>
         <span class="title">账户设置</span>
       </header>
+      <split></split>
+      <div class="ui-user">
+        <div class="ui-avatar"><img :src="user['avatar']" alt=""></div>
+        <div class="ui-info">
+          <div class="ui-info-item">{{user['nickname']}}</div>
+          <div class="ui-info-item ui-info-username">用户名: {{user['username']}}</div>
+        </div>
+      </div>
       <split></split>
       <div @click="logout" class="u-logout">
         退出登录
@@ -98,6 +106,34 @@
         display inline-block
         height 3em
         line-height 3em
+    .ui-user
+      padding 1em 0
+      width 100%
+      text-align center
+      .ui-avatar
+        display inline-block
+        width 3em
+        height 3em
+        overflow hidden
+        border-radius 50%
+        img
+          width 100%
+          height 100%
+      .ui-info
+        display inline-block
+        text-align left
+        height 3em
+        line-height 3em
+        vertical-align top
+        margin-left 1em
+        overflow hidden
+        .ui-info-item
+          width 100%
+          height 1.5em
+          line-height 1.5em
+          vertical-align top
+          &.ui-info-username
+            color #aaa
     .u-logout
       width 100%
       height 3em
